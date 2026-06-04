479411a568058381309a7d7386a47e8791fb89dc
savage1590 <vladprotsenko2@gmail.com>
2026-06-04 22:18:24 +0300

    chore: replace GA tag with Google Ads tag AW-18214434789

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -137,9 +137,9 @@ export default async function RootLayout({
         </NextIntlClientProvider>
         <Analytics />
         
-        {/* Google Analytics */}
+        {/* Google Analytics / Ads */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-BV9D2QK1E0"
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18214434789"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
@@ -147,7 +147,7 @@ export default async function RootLayout({
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-BV9D2QK1E0');
+            gtag('config', 'AW-18214434789');
           `}
         </Script>
       </body>
